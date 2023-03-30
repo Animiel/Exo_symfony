88295812118557fefd8669059a2cd7d5430b81a5
Auth_symfony <florian.leininger01@gmail.com>
2023-03-30 16:56:55 +0200

    + init entités

---
+++ src/Entity/Session.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SessionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SessionRepository::class)]
+class Session
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $intitule = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateDebut = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateFin = null;
+
+    #[ORM\Column]
+    private ?int $nbPlaces = null;
+
+    #[ORM\Column]
+    private ?int $placesReservees = null;
+
+    #[ORM\Column]
+    private ?int $placesLibres = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIntitule(): ?string
+    {
+        return $this->intitule;
+    }
+
+    public function setIntitule(string $intitule): self
+    {
+        $this->intitule = $intitule;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $dateDebut): self
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(\DateTimeInterface $dateFin): self
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    public function getNbPlaces(): ?int
+    {
+        return $this->nbPlaces;
+    }
+
+    public function setNbPlaces(int $nbPlaces): self
+    {
+        $this->nbPlaces = $nbPlaces;
+
+        return $this;
+    }
+
+    public function getPlacesReservees(): ?int
+    {
+        return $this->placesReservees;
+    }
+
+    public function setPlacesReservees(int $placesReservees): self
+    {
+        $this->placesReservees = $placesReservees;
+
+        return $this;
+    }
+
+    public function getPlacesLibres(): ?int
+    {
+        return $this->placesLibres;
+    }
+
+    public function setPlacesLibres(int $placesLibres): self
+    {
+        $this->placesLibres = $placesLibres;
+
+        return $this;
+    }
+}
