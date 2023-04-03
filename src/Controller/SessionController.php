3a810c34035c79e0ceb04924d9e48d01d9d28c1f
Exo_symfony <florian.leininger01@gmail.com>
2023-04-03 14:21:21 +0200

    + suppr stagiaire et module dans session

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Programme;
+use App\Entity\Stagiaire;
 use App\Entity\SessionFormation;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class SessionController extends AbstractController
 {
@@ -19,6 +22,7 @@ class SessionController extends AbstractController
         ]);
     }
 
+    //quand un seul parametre --> lien automatique ente id et $session
     #[Route('/session/{id}', name: 'detail_session')]
     public function detail_session(SessionFormation $session) {
 
@@ -30,8 +34,31 @@ class SessionController extends AbstractController
     #[Route('/session/{id}/programme', name: 'programme_session')]
     public function programmeSession(SessionFormation $session) {
 
-        return $this->render('session/programme.html.twig', [
+        return $this->render('session/detailSession.html.twig', [
             'session' => $session
         ]);
+    }
+
+    //quand plusieurs param --> utiliser Paramconverter pour bien faire le lien
+    #[Route('/session/{idSe}/delete/{idSt}', name: 'delete_stagiaire')]
+    #[ParamConverter("session", options:["mapping" => ["idSe" => "id"]])]
+    #[ParamConverter("stagiaire", options:["mapping" => ["idSt" => "id"]])]
+    public function deleteStagiaire(ManagerRegistry $doctrine, Stagiaire $stagiaire, SessionFormation $session) {
+
+        $session->removeStagiaire($stagiaire);
+        $em = $doctrine->getManager();
+        $em->flush();
+        return $this->redirectToRoute('detail_session', ['id' => $session->getId()]);
+    }
+
+    #[Route('/session/{idSe}/{idProg}/delete', name: 'delete_programme')]
+    #[ParamConverter("session", options:["mapping" => ["idSe" => "id"]])]
+    #[ParamConverter("programme", options:["mapping" => ["idProg" => "id"]])]
+    public function deleteModule(ManagerRegistry $doctrine, Programme $programme, SessionFormation $session) {
+
+        $session->removeProgramme($programme);
+        $em = $doctrine->getManager();
+        $em->flush();
+        return $this->redirectToRoute('detail_session', ['id' => $session->getId()]);
     }
 }
