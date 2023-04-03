d6ed8e06d0896583b444456987bf7b88d92c3606
Exo_symfony <florian.leininger01@gmail.com>
2023-04-03 10:19:32 +0200

    + liste stagiaires

---
+++ src/Controller/StagiaireController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Stagiaire;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class StagiaireController extends AbstractController
+{
+    #[Route('/stagiaire', name: 'app_stagiaire')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $stagiaires = $doctrine->getRepository(Stagiaire::class)->findAll();
+        return $this->render('stagiaire/index.html.twig', [
+            'stagiaires' => $stagiaires,
+        ]);
+    }
+
+    #[Route('/stagiaire/{id}', name: 'profil_stagiaire')]
+    public function profilStagiaire() {
+        return $this->render('stagiaire/profilStagiaire.html.twig', [
+        ]);
+    }
+}
