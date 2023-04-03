a645c68a77b58581097e3b52eb0718c0300bc4a9
Exo_symfony <florian.leininger01@gmail.com>
2023-04-03 15:48:58 +0200

    + form add stagiaire

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Stagiaire;
+use App\Form\StagiaireType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,6 +21,32 @@ class StagiaireController extends AbstractController
         ]);
     }
 
+    
+    #[Route('/stagiaire/add', name: 'add_stagiaire')]
+    public function addStagiaire(ManagerRegistry $doctrine, Stagiaire $stagiaire = null, Request $request) {
+        
+        if (!$stagiaire) {
+            $stagiaire = new Stagiaire();
+        }
+        
+        $form = $this->createForm(StagiaireType::class, $stagiaire);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $stagiaire = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($stagiaire);
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('app_stagiaire');
+        }
+        
+        return $this->render('stagiaire/add.html.twig', [
+            'formStagiaire' => $form->createView(),
+        ]);
+    }
+    
+    //toujours en fin pour éviter les conflits (ex: id = add => inexistant)
     #[Route('/stagiaire/{id}', name: 'profil_stagiaire')]
     public function profilStagiaire(Stagiaire $stagiaire) {
         return $this->render('stagiaire/profilStagiaire.html.twig', [
